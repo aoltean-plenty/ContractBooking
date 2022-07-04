f27c472be92f5a34ccf1b6dd53398f9033310c9b
aoltean-plenty <alexandru.oltean@plentymarkets.com>
2022-07-04 17:02:54 +0300

    Folder restructure

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -73,7 +73,6 @@
           v-model="addressLine3"/>
     </div>
 
-
       <div :class="[{'col-md-3': 'default-grid', 'form-group--error': v$.postCode.$error }]">
       <BaseInput
           label="Postcode"
@@ -131,17 +130,46 @@
         <ErrorMessage v-if="v$.password.$error" v-text="v$.password.$errors[0].$message"/>
       </div>
 
+
+      <div :class="[{'col-md-12': 'default-grid', 'form-group--error': v$.accountOwner.$error }]">
+        <BaseInput
+            label="Account Owner"
+            type="text"
+            id="accountName"
+            v-model="accountOwner"/>
+        <ErrorMessage v-if="v$.accountOwner.$error" v-text="v$.accountOwner.$errors[0].$message"/>
+      </div>
+
+      <div :class="[{'col-md-12': 'default-grid', 'form-group--error': v$.accountNumber.$error }]">
+        <BaseInput
+            label="Account Number"
+            type="text"
+            id="accountNumberInput"
+            v-model="accountNumber"/>
+        <ErrorMessage v-if="v$.accountNumber.$error" v-text="v$.accountNumber.$errors[0].$message"/>
+      </div>
+
+      <div :class="[{'col-md-12': 'default-grid', 'form-group--error': v$.sortCode.$error }]">
+        <BaseInput
+            label="Sort code"
+            type="number"
+            id="sortCodeInput"
+            v-model="sortCode"/>
+        <ErrorMessage v-if="v$.sortCode.$error" v-text="v$.sortCode.$errors[0].$message"/>
+      </div>
+
       <BaseInput type="submit"/>
+
     </div>
   </form>
 </template>
 
 <script>
 import useVuelidate from '@vuelidate/core'
-import { helpers,required,email,minLength,sameAs } from '@vuelidate/validators'
-import BaseInput from "./BaseInput.vue";
-import SelectField from "./SelectField.vue";
-import ErrorMessage from "./ErrorMessage.vue";
+import { required,email,minLength,sameAs } from '@vuelidate/validators'
+import BaseInput from "./Inputs/BaseInput.vue";
+import SelectField from "./Inputs/SelectField.vue";
+import ErrorMessage from "./Errors/ErrorMessage.vue";
 import '@mdi/font/css/materialdesignicons.css';
 
 export default {
@@ -172,7 +200,11 @@ export default {
       password:{
         password: '',
         repeat: '',
-      }
+      },
+
+      accountOwner:'',
+      accountNumber: '',
+      sortCode: '',
     }
   },
 
@@ -191,22 +223,16 @@ export default {
       password:{
         password: {required,minLength: minLength(8),},
         repeat: {required, sameAs: sameAs(this.password.password)},
-      }
+      },
+      accountOwner: {required},
+      accountNumber: {required},
+      sortCode: {required},
     }
   },
 
   methods:{
     optionUpdate(value){
       this.country = value;
-    },
-
-
-    // showPassword(){
-    //   this.visibility = 'text';
-    // },
-
-    hidePassword(){
-      this.visibility = 'passowrd';
     },
 
      submitForm(){
